3f506388b98f9f38020daf31a5fb9432b8e4a29f
张乾泽 <qianze.zhang@hotmail.com>
2018-11-13 17:57:13 +0800

    sample code fix

--- DynamicKey/AgoraDynamicKey/php/sample/AccessTokenSample.php
+++ DynamicKey/AgoraDynamicKey/php/sample/AccessTokenSample.php
@@ -8,8 +8,7 @@ $uid = 2882341273;
 $expireTimestamp = 0;
 
 
-$builder = new AccessToken();
-$builder->init($appID, $appCertificate, $channelName, $uid);
+$builder = AccessToken::init($appID, $appCertificate, $channelName, $uid);
 $builder->addPrivilege(AccessToken::Privileges["kJoinChannel"], $expireTimestamp);
 echo $builder->build();
 
