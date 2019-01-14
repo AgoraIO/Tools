8c7e583f57029ef1143ea999b0387f796c1b2684
Haonong Yu <yuhaonong@agora.io>
2019-01-14 16:49:45 +0800

    remove unused variable

--- DynamicKey/AgoraDynamicKey/java/sample/io/agora/media/sample/RtmTokenBuilderSample.java
+++ DynamicKey/AgoraDynamicKey/java/sample/io/agora/media/sample/RtmTokenBuilderSample.java
@@ -6,7 +6,6 @@ import io.agora.media.RtmTokenBuilder;
 public class RtmTokenBuilderSample {
     static String appId = "970CA35de60c44645bbae8a215061b33";
     static String appCertificate = "5CFd2fd1755d40ecb72977518be15d3b";
-    static String channelName = "7d72365eb983485397e3e3f9d460bdda";
     static String userId = "2882341273";
     static int expireTimestamp = 0;
 
