37f25ba3bdf99bc2293a87f19572a7fa0deed69c
linshibo <linshibo@gmail.com>
2019-07-16 09:07:47 +0000

    fix cpp sample code

--- DynamicKey/AgoraDynamicKey/cpp/sample/sample_builder.cpp
+++ DynamicKey/AgoraDynamicKey/cpp/sample/sample_builder.cpp
@@ -1,8 +1,8 @@
 /**
  * build with command:
- * g++ -std=c++0x -O0 -I../../ sample_builder2.cpp  -lz -lcrypto -o sample_builder2
+ * g++ -std=c++0x -O0 -I../../ sample_builder.cpp  -lz -lcrypto -o sample_builder
  */
-#include "../src/SimpleTokenBuilder2.h"
+#include "../src/SimpleTokenBuilder.h"
 #include <iostream>
 #include <cstdint>
 using namespace agora::tools;
@@ -14,13 +14,17 @@ int main(int argc, char const *argv[]) {
   std::string channelName= "7d72365eb983485397e3e3f9d460bdda";
   uint32_t uid = 2882341273;
   std::string userAccount = "2882341273";
-  uint32_t expireTimestamp = 0;
+  uint32_t expirationTimeInSeconds = 3600;
+  uint32_t currentTimeStamp = time(NULL);
+  uint32_t privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds;
   std::string result;
-  result = SimpleTokenBuilder2::buildTokenWithUid(
-      appID, appCertificate, channelName, uid, UserRole::Role_Attendee, expireTimestamp);
-  std::cout << result << std::endl;
-  result = SimpleTokenBuilder2::buildTokenWithUserAccount(
-      appID, appCertificate, channelName, userAccount, UserRole::Role_Attendee, expireTimestamp);
-  std::cout << result << std::endl;
+  result = SimpleTokenBuilder::buildTokenWithUid(
+      appID, appCertificate, channelName, uid, UserRole::Role_Attendee,
+      privilegeExpiredTs);
+  std::cout << "Token With Int Uid:" << result << std::endl;
+  result = SimpleTokenBuilder::buildTokenWithUserAccount(
+      appID, appCertificate, channelName, userAccount, UserRole::Role_Attendee,
+      privilegeExpiredTs);
+  std::cout << "Token With UserAccount:" << result << std::endl;
   return 0;
 }
