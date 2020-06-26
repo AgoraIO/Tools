5085d0c25865e5bd887a1620e91c3c53de7692f9
Adam Wan <adamaikenadam@gmail.com>
2020-06-26 14:49:06 +0800

    fix incorrect expiration in sample

--- DynamicKey/AgoraDynamicKey/ruby/sample/rtm_token_builder_sample.rb
+++ DynamicKey/AgoraDynamicKey/ruby/sample/rtm_token_builder_sample.rb
@@ -9,7 +9,7 @@ params = {
   app_certificate: '5CFd2fd1755d40ecb72977518be15d3b',
   account: "test_user_id",
   role: AgoraDynamicKey::RTMTokenBuilder::Role::RTM_USER,
-  privilege_expired_ts: current_time_stamps - expiration_time_in_seconds
+  privilege_expired_ts: current_time_stamps + expiration_time_in_seconds
 }
 
 result = AgoraDynamicKey::RTMTokenBuilder.build_token params
