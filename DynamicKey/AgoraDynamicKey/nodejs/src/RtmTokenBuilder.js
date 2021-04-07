261dd857c96d7c7be8b273c93b47536483a3e8a7
Atish Abhang <atishabhang@live.com>
2021-04-07 15:44:50 +0530

    fixed wrong argument passed

--- DynamicKey/AgoraDynamicKey/nodejs/src/RtmTokenBuilder.js
+++ DynamicKey/AgoraDynamicKey/nodejs/src/RtmTokenBuilder.js
@@ -12,16 +12,14 @@ class RtmTokenBuilder {
    * @param {*} appCertificate:	Certificate of the application that you registered in 
    *                 the Agora Dashboard. See Get an App Certificate.
    * @param {*} account: The user account. 
-   * @param {*} role : Role_Publisher = 1: A broadcaster (host) in a live-broadcast profile.
-   *      Role_Subscriber = 2: (Default) A audience in a live-broadcast profile.
    * @param {*} privilegeExpiredTs : represented by the number of seconds elapsed since 
    *                   1/1/1970. If, for example, you want to access the
    *                   Agora Service within 10 minutes after the token is 
    *                   generated, set expireTimestamp as the current 
    * @return token
    */
-  static buildToken (appID, appCertificate, account, role, privilegeExpiredTs) {
-    const key = new AccessToken(appID, appCertificate, account, "")
+  static buildToken(appID, appCertificate, account, privilegeExpiredTs) {
+    const key = new AccessToken(appID, appCertificate, '', account)
     key.addPriviledge(Priviledges.kRtmLogin, privilegeExpiredTs)
     return key.build()
   }
