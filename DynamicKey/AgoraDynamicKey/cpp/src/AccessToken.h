4e7c2d44124381c7847f6589b7fe14e430234863
xianing <xianing@agora.io>
2021-07-10 21:52:31 +0800

    remove uselesss privileges

--- DynamicKey/AgoraDynamicKey/cpp/src/AccessToken.h
+++ DynamicKey/AgoraDynamicKey/cpp/src/AccessToken.h
@@ -18,15 +18,6 @@ struct AccessToken {
     kPublishVideoStream = 3,
     kPublishDataStream = 4,
 
-    kPublishAudiocdn = 5,
-    kPublishVideoCdn = 6,
-    kRequestPublishAudioStream = 7,
-    kRequestPublishVideoStream = 8,
-    kRequestPublishDataStream = 9,
-    kInvitePublishAudioStream = 10,
-    kInvitePublishVideoStream = 11,
-    kInvitePublishDataStream = 12,
-
     kAdministrateChannel = 101,
 
     kRtmLogin = 1000,
