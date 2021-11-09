34e5c0b896c780637c28b4d8c7fb79be83183eba
lixinhui <lixinhui@agora.io>
2021-11-09 19:01:05 +0800

    add ut for RtcTokenBuilder2

--- DynamicKey/AgoraDynamicKey/nodejs/src/AccessToken2.js
+++ DynamicKey/AgoraDynamicKey/nodejs/src/AccessToken2.js
@@ -1,42 +1,43 @@
-var crypto = require('crypto');
-const zlib = require('zlib');
-var UINT32 = require('cuint').UINT32;
-const VERSION_LENGTH = 3;
-const APP_ID_LENGTH = 32;
+var crypto = require('crypto')
+const zlib = require('zlib')
+const VERSION_LENGTH = 3
+const APP_ID_LENGTH = 32
 
 const getVersion = () => {
     return "007"
 }
 
 class Service {
-    constructor(service_type){
+    constructor(service_type) {
         this.__type = service_type
         this.__privileges = {}
     }
 
-    __pack_type(){
+    __pack_type() {
         let buf = new ByteBuf()
         buf.putUint16(this.__type)
         return buf.pack()
     }
 
-    __pack_privileges(){
+    __pack_privileges() {
         let buf = new ByteBuf()
         buf.putTreeMapUInt32(this.__privileges)
         return buf.pack()
     }
 
-    service_type(){
+    service_type() {
         return this.__type
     }
 
-    add_privilege(privilege, expire){
+    add_privilege(privilege, expire) {
         this.__privileges[privilege] = expire
     }
-    pack(){
+
+    pack() {
         return Buffer.concat([this.__pack_type(), this.__pack_privileges()])
     }
-    unpack(buffer){
+
+    unpack(buffer) {
         let bufReader = new ReadByteBuf(buffer)
         this.__privileges = bufReader.getTreeMapUInt32()
         return bufReader
@@ -44,79 +45,85 @@ class Service {
 }
 
 const kRtcServiceType = 1
-class ServiceRtc extends Service{
+
+class ServiceRtc extends Service {
     constructor(channel_name, uid) {
         super(kRtcServiceType)
         this.__channel_name = channel_name
         this.__uid = uid === 0 ? '' : `${uid}`
     }
 
-    pack(){
+    pack() {
         let buffer = new ByteBuf()
         buffer.putString(this.__channel_name).putString(this.__uid)
-        return Buffer.concat([super.pack(),buffer.pack()])
+        return Buffer.concat([super.pack(), buffer.pack()])
     }
 
-    unpack(buffer){
+    unpack(buffer) {
         let bufReader = super.unpack(buffer)
         this.__channel_name = bufReader.getString()
         this.__uid = bufReader.getString()
         return bufReader
     }
 }
+
 ServiceRtc.kPrivilegeJoinChannel = 1
 ServiceRtc.kPrivilegePublishAudioStream = 2
 ServiceRtc.kPrivilegePublishVideoStream = 3
 ServiceRtc.kPrivilegePublishDataStream = 4
 
 const kRtmServiceType = 2
-class ServiceRtm extends Service{
+
+class ServiceRtm extends Service {
     constructor(user_id) {
         super(kRtmServiceType)
         this.__user_id = user_id || ""
     }
 
-    pack(){
+    pack() {
         let buffer = new ByteBuf()
         buffer.putString(this.__user_id)
-        return Buffer.concat([super.pack(),buffer.pack()])
+        return Buffer.concat([super.pack(), buffer.pack()])
     }
 
-    unpack(buffer){
+    unpack(buffer) {
         let bufReader = super.unpack(buffer)
         this.__user_id = bufReader.getString()
         return bufReader
     }
 }
+
 ServiceRtm.kPrivilegeLogin = 1
 
 const kChatServiceType = 5
-class ServiceChat extends Service{
+
+class ServiceChat extends Service {
     constructor(user_id) {
         super(kChatServiceType)
         this.__user_id = user_id || ""
     }
 
-    pack(){
+    pack() {
         let buffer = new ByteBuf()
         buffer.putString(this.__user_id)
-        return Buffer.concat([super.pack(),buffer.pack()])
+        return Buffer.concat([super.pack(), buffer.pack()])
     }
 
-    unpack(buffer){
+    unpack(buffer) {
         super.unpack(buffer)
         this.__user_id = buffer.getString()
         return buffer
     }
 }
+
 ServiceChat.kPrivilegeUser = 1
 ServiceChat.kPrivilegeApp = 2
 
-class AccessToken2{
-    constructor(appID, appCertificate, issue_ts, expire) {
-        this.appID = appID
+class AccessToken2 {
+    constructor(appId, appCertificate, issueTs, expire) {
+        this.appId = appId
         this.appCertificate = appCertificate
-        this.issue_ts = issue_ts || new Date().getTime()/1000
+        this.issueTs = issueTs || new Date().getTime() / 1000
         this.expire = expire
         // salt ranges in (1, 99999999)
         this.salt = Math.floor(Math.random() * (99999999)) + 1
@@ -124,59 +131,59 @@ class AccessToken2{
     }
 
     __signing() {
-        let signing = encodeHMac(new ByteBuf().putUint32(this.issue_ts).pack(), this.appCertificate)
-        signing = encodeHMac(new ByteBuf().putUint32(this.salt).pack(), signing )
+        let signing = encodeHMac(new ByteBuf().putUint32(this.issueTs).pack(), this.appCertificate)
+        signing = encodeHMac(new ByteBuf().putUint32(this.salt).pack(), signing)
         return signing
     }
 
     __build_check() {
         let is_uuid = (data) => {
-            if(data.length !== APP_ID_LENGTH) {
+            if (data.length !== APP_ID_LENGTH) {
                 return false
             }
             let buf = Buffer.from(data, 'hex')
             return !!buf
         }
 
-        const {appID, appCertificate, services} = this
-        if(!is_uuid(appID) || !is_uuid(appCertificate)) {
+        const {appId, appCertificate, services} = this
+        if (!is_uuid(appId) || !is_uuid(appCertificate)) {
             return false
         }
 
-        if(Object.keys(services).length === 0){
+        if (Object.keys(services).length === 0) {
             return false
         }
         return true
     }
 
-    add_service(service){
+    add_service(service) {
         this.services[service.service_type()] = service
     }
 
-    build(){
-        if(!this.__build_check()){
+    build() {
+        if (!this.__build_check()) {
             return ""
         }
 
         let signing = this.__signing()
-        let signing_info = new ByteBuf().putString(this.appID)
-                .putUint32(this.issue_ts)
-                .putUint32(this.expire)
-                .putUint32(this.salt)
-                .putUint16(Object.keys(this.services).length).pack()
+        let signing_info = new ByteBuf().putString(this.appId)
+            .putUint32(this.issueTs)
+            .putUint32(this.expire)
+            .putUint32(this.salt)
+            .putUint16(Object.keys(this.services).length).pack()
         Object.values(this.services).forEach(service => {
             signing_info = Buffer.concat([signing_info, service.pack()])
         })
 
         let signature = encodeHMac(signing, signing_info)
-        let content = Buffer.concat([new ByteBuf().putString(signature).pack(),signing_info])
+        let content = Buffer.concat([new ByteBuf().putString(signature).pack(), signing_info])
         let compressed = zlib.deflateSync(content)
         return `${getVersion()}${Buffer.from(compressed).toString('base64')}`
     }
 
     from_string(origin_token) {
         let origin_version = origin_token.substring(0, VERSION_LENGTH)
-        if(origin_version !== getVersion()){
+        if (origin_version !== getVersion()) {
             return false
         }
 
@@ -185,187 +192,143 @@ class AccessToken2{
         let bufferReader = new ReadByteBuf(buffer)
 
         let signature = bufferReader.getString()
-        this.appID = bufferReader.getString()
-        this.issue_ts = bufferReader.getUint32()
+        this.appId = bufferReader.getString()
+        this.issueTs = bufferReader.getUint32()
         this.expire = bufferReader.getUint32()
         this.salt = bufferReader.getUint32()
         let service_count = bufferReader.getUint16()
 
         let remainBuf = bufferReader.pack()
-        for(let i = 0; i < service_count; i++) {
-            let service_type = bufferReader.getUint16()
+        for (let i = 0; i < service_count; i++) {
+            let bufferReaderService = new ReadByteBuf(remainBuf)
+            let service_type = bufferReaderService.getUint16()
             let service = new AccessToken2.kServices[service_type]()
-            remainBuf = service.unpack(remainBuf).pack()
+            remainBuf = service.unpack(bufferReaderService.pack()).pack()
             this.services[service_type] = service
         }
     }
 }
 
-
 var encodeHMac = function (key, message) {
-    return crypto.createHmac('sha256', key).update(message).digest();
-};
+    return crypto.createHmac('sha256', key).update(message).digest()
+}
 
 var ByteBuf = function () {
     var that = {
         buffer: Buffer.alloc(1024)
         , position: 0
-    };
+    }
 
-    that.buffer.fill(0);
+    that.buffer.fill(0)
 
     that.pack = function () {
-        var out = Buffer.alloc(that.position);
-        that.buffer.copy(out, 0, 0, out.length);
-        return out;
-    };
+        var out = Buffer.alloc(that.position)
+        that.buffer.copy(out, 0, 0, out.length)
+        return out
+    }
 
     that.putUint16 = function (v) {
-        that.buffer.writeUInt16LE(v, that.position);
-        that.position += 2;
-        return that;
-    };
+        that.buffer.writeUInt16LE(v, that.position)
+        that.position += 2
+        return that
+    }
 
     that.putUint32 = function (v) {
-        that.buffer.writeUInt32LE(v, that.position);
-        that.position += 4;
-        return that;
-    };
+        that.buffer.writeUInt32LE(v, that.position)
+        that.position += 4
+        return that
+    }
 
     that.putBytes = function (bytes) {
-        that.putUint16(bytes.length);
-        bytes.copy(that.buffer, that.position);
-        that.position += bytes.length;
-        return that;
-    };
+        that.putUint16(bytes.length)
+        bytes.copy(that.buffer, that.position)
+        that.position += bytes.length
+        return that
+    }
 
     that.putString = function (str) {
-        return that.putBytes(Buffer.from(str));
-    };
+        return that.putBytes(Buffer.from(str))
+    }
 
     that.putTreeMap = function (map) {
         if (!map) {
-            that.putUint16(0);
-            return that;
+            that.putUint16(0)
+            return that
         }
 
-        that.putUint16(Object.keys(map).length);
+        that.putUint16(Object.keys(map).length)
         for (var key in map) {
-            that.putUint16(key);
-            that.putString(map[key]);
+            that.putUint16(key)
+            that.putString(map[key])
         }
 
-        return that;
-    };
+        return that
+    }
 
     that.putTreeMapUInt32 = function (map) {
         if (!map) {
-            that.putUint16(0);
-            return that;
+            that.putUint16(0)
+            return that
         }
 
-        that.putUint16(Object.keys(map).length);
+        that.putUint16(Object.keys(map).length)
         for (var key in map) {
-            that.putUint16(key);
-            that.putUint32(map[key]);
+            that.putUint16(key)
+            that.putUint32(map[key])
         }
 
-        return that;
-    };
-    
-    return that;
+        return that
+    }
+
+    return that
 }
 
-
-var ReadByteBuf = function(bytes) {
+var ReadByteBuf = function (bytes) {
     var that = {
         buffer: bytes
         , position: 0
-    };
+    }
 
     that.getUint16 = function () {
-        var ret = that.buffer.readUInt16LE(that.position);
-        that.position += 2;
-        return ret;
-    };
+        var ret = that.buffer.readUInt16LE(that.position)
+        that.position += 2
+        return ret
+    }
 
     that.getUint32 = function () {
-        var ret = that.buffer.readUInt32LE(that.position);
-        that.position += 4;
-        return ret;
-    };
+        var ret = that.buffer.readUInt32LE(that.position)
+        that.position += 4
+        return ret
+    }
 
     that.getString = function () {
-        var len = that.getUint16();
+        var len = that.getUint16()
 
-        var out = Buffer.alloc(len);
-        that.buffer.copy(out, 0, that.position, (that.position + len));
-        that.position += len;
-        return out;
-    };
+        var out = Buffer.alloc(len)
+        that.buffer.copy(out, 0, that.position, (that.position + len))
+        that.position += len
+        return out
+    }
 
     that.getTreeMapUInt32 = function () {
-        var map = {};
-        var len = that.getUint16();
-        for( var i = 0; i < len; i++) {
-            var key = that.getUint16();
-            var value = that.getUint32();
-            map[key] = value;
+        var map = {}
+        var len = that.getUint16()
+        for (var i = 0; i < len; i++) {
+            var key = that.getUint16()
+            var value = that.getUint32()
+            map[key] = value
         }
-        return map;
-    };
+        return map
+    }
 
     that.pack = function () {
         let length = that.buffer.length
-        var out = Buffer.alloc(length - that.position - 1);
-        that.buffer.copy(out, 0, that.position, out.length);
-        return out;
-    };
-
-    return that;
-}
-var AccessTokenContent = function (options) {
-    options.pack = function () {
-        var out = new ByteBuf();
-        return out.putString(options.signature)
-            .putUint32(options.crc_channel)
-            .putUint32(options.crc_uid)
-            .putString(options.m).pack();
+        var out = Buffer.alloc(length)
+        that.buffer.copy(out, 0, that.position, length)
+        return out
     }
 
-    return options;
-}
-
-var Message = function (options) {
-    options.pack = function () {
-        var out = new ByteBuf();
-        var val = out
-            .putUint32(options.salt)
-            .putUint32(options.ts)
-            .putTreeMapUInt32(options.messages).pack();
-        return val;
-    }
-
-    return options;
-}
-
-var unPackContent = function(bytes) {
-    var readbuf = new ReadByteBuf(bytes);
-    return AccessTokenContent({
-        signature: readbuf.getString(),
-        crc_channel_name: readbuf.getUint32(),
-        crc_uid: readbuf.getUint32(),
-        m: readbuf.getString()
-    });
-}
-
-var unPackMessages = function(bytes) {
-    var readbuf = new ReadByteBuf(bytes);
-    return Message({
-        salt: readbuf.getUint32(),
-        ts: readbuf.getUint32(),
-        messages: readbuf.getTreeMapUInt32()
-    });
+    return that
 }
 
 AccessToken2.kServices = {}
@@ -373,4 +336,7 @@ AccessToken2.kServices[kRtcServiceType] = ServiceRtc
 AccessToken2.kServices[kRtmServiceType] = ServiceRtm
 AccessToken2.kServices[kChatServiceType] = ServiceChat
 
-module.exports = {AccessToken2, ServiceRtc, ServiceRtm, ServiceChat}
+module.exports = {
+    AccessToken2, ServiceRtc, ServiceRtm, ServiceChat,
+    kRtcServiceType, kRtmServiceType, kChatServiceType
+}
--- DynamicKey/AgoraDynamicKey/nodejs/src/RtcTokenBuilder2.js
+++ DynamicKey/AgoraDynamicKey/nodejs/src/RtcTokenBuilder2.js
@@ -1,74 +1,68 @@
-const AccessToken = require('../src/AccessToken2').AccessToken
+const AccessToken = require('../src/AccessToken2').AccessToken2
+const ServiceRtc = require('../src/AccessToken2').ServiceRtc
 
 const Role = {
-    // DEPRECATED. Role::ATTENDEE has the same privileges as Role.PUBLISHER.
-    ATTENDEE: 0,
-
-    // RECOMMENDED. Use this role for a voice/video call or a live broadcast, if your scenario does not require authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in).
+    // for live broadcaster
     PUBLISHER: 1,
 
-    /* Only use this role if your scenario require authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in).
-     * @note In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role.SUBSCRIBER still has the same privileges as Role.PUBLISHER.
-     */
+    // default, for live audience
     SUBSCRIBER: 2,
-
-    // DEPRECATED. Role.ADMIN has the same privileges as Role.PUBLISHER.
-    ADMIN: 101
 }
 
 class RtcTokenBuilder {
-
-   /**
-    * Builds an RTC token using an Integer uid.
-    * @param {*} appID  The App ID issued to you by Agora.
-    * @param {*} appCertificate Certificate of the application that you registered in the Agora Dashboard.
-    * @param {*} channelName The unique channel name for the AgoraRTC session in the string format. The string length must be less than 64 bytes. Supported character scopes are:
-    * - The 26 lowercase English letters: a to z.
-    * - The 26 uppercase English letters: A to Z.
-    * - The 10 digits: 0 to 9.
-    * - The space.
-    * - "!", "#", "$", "%", "&", "(", ")", "+", "-", ":", ";", "<", "=", ".", ">", "?", "@", "[", "]", "^", "_", " {", "}", "|", "~", ",".
-    * @param {*} uid User ID. A 32-bit unsigned integer with a value ranging from 1 to (2^32-1).
-    * @param {*} role See #userRole.
-    * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
-    * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
-    * @param {*} privilegeExpiredTs  represented by the number of seconds elapsed since 1/1/1970. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set expireTimestamp as the current timestamp + 600 (seconds).
-    * @return The new Token.
-    */
-    static buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs) {
-        return this.buildTokenWithAccount(appID, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    /**
+     * Builds an RTC token using an Integer uid.
+     * @param {*} appId  The App ID issued to you by Agora.
+     * @param {*} appCertificate Certificate of the application that you registered in the Agora Dashboard.
+     * @param {*} channelName The unique channel name for the AgoraRTC session in the string format. The string length must be less than 64 bytes. Supported character scopes are:
+     * - The 26 lowercase English letters: a to z.
+     * - The 26 uppercase English letters: A to Z.
+     * - The 10 digits: 0 to 9.
+     * - The space.
+     * - "!", "#", "$", "%", "&", "(", ")", "+", "-", ":", ";", "<", "=", ".", ">", "?", "@", "[", "]", "^", "_", " {", "}", "|", "~", ",".
+     * @param {*} uid User ID. A 32-bit unsigned integer with a value ranging from 1 to (2^32-1).
+     * @param {*} role See #userRole.
+     * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
+     * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
+     * @param {*} expire represented by the number of seconds elapsed since 1/1/1970. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set expireTimestamp as the current timestamp + 600 (seconds).
+     * @return The new Token.
+     */
+    static buildTokenWithUid(appId, appCertificate, channelName, uid, role, expire) {
+        return this.buildTokenWithAccount(appId, appCertificate, channelName, uid, role, expire)
     }
 
-   /**
-    * Builds an RTC token using an Integer uid.
-    * @param {*} appID  The App ID issued to you by Agora.
-    * @param {*} appCertificate Certificate of the application that you registered in the Agora Dashboard.
-    * @param {*} channelName The unique channel name for the AgoraRTC session in the string format. The string length must be less than 64 bytes. Supported character scopes are:
-    * - The 26 lowercase English letters: a to z.
-    * - The 26 uppercase English letters: A to Z.
-    * - The 10 digits: 0 to 9.
-    * - The space.
-    * - "!", "#", "$", "%", "&", "(", ")", "+", "-", ":", ";", "<", "=", ".", ">", "?", "@", "[", "]", "^", "_", " {", "}", "|", "~", ",".
-    * @param {*} account The user account.
-    * @param {*} role See #userRole.
-    * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
-    * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
-    * @param {*} privilegeExpiredTs  represented by the number of seconds elapsed since 1/1/1970. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set expireTimestamp as the current timestamp + 600 (seconds).
-    * @return The new Token.
-    */
-    static buildTokenWithAccount(appID, appCertificate, channelName, account, role, privilegeExpiredTs) {
-        let key = new AccessToken(appID, appCertificate, null, privilegeExpiredTs)
-        // this.key.addPriviledge(Priviledges.kJoinChannel, privilegeExpiredTs)
-        // if (role == Role.ATTENDEE ||
-        //     role == Role.PUBLISHER ||
-        //     role == Role.ADMIN) {
-        //     this.key.addPriviledge(Priviledges.kPublishAudioStream, privilegeExpiredTs)
-        //     this.key.addPriviledge(Priviledges.kPublishVideoStream, privilegeExpiredTs)
-        //     this.key.addPriviledge(Priviledges.kPublishDataStream, privilegeExpiredTs)
-        // }
-        return key.build();
+    /**
+     * Builds an RTC token using an Integer uid.
+     * @param {*} appId  The App ID issued to you by Agora.
+     * @param {*} appCertificate Certificate of the application that you registered in the Agora Dashboard.
+     * @param {*} channelName The unique channel name for the AgoraRTC session in the string format. The string length must be less than 64 bytes. Supported character scopes are:
+     * - The 26 lowercase English letters: a to z.
+     * - The 26 uppercase English letters: A to Z.
+     * - The 10 digits: 0 to 9.
+     * - The space.
+     * - "!", "#", "$", "%", "&", "(", ")", "+", "-", ":", ";", "<", "=", ".", ">", "?", "@", "[", "]", "^", "_", " {", "}", "|", "~", ",".
+     * @param {*} account The user account.
+     * @param {*} role See #userRole.
+     * - Role.PUBLISHER; RECOMMENDED. Use this role for a voice/video call or a live broadcast.
+     * - Role.SUBSCRIBER: ONLY use this role if your live-broadcast scenario requires authentication for [Hosting-in](https://docs.agora.io/en/Agora%20Platform/terms?platform=All%20Platforms#hosting-in). In order for this role to take effect, please contact our support team to enable authentication for Hosting-in for you. Otherwise, Role_Subscriber still has the same privileges as Role_Publisher.
+     * @param {*} expire represented by the number of seconds elapsed since 1/1/1970. If, for example, you want to access the Agora Service within 10 minutes after the token is generated, set expireTimestamp as the current timestamp + 600 (seconds).
+     * @return The new Token.
+     */
+    static buildTokenWithAccount(appId, appCertificate, channelName, account, role, expire) {
+        let token = new AccessToken(appId, appCertificate, null, expire)
+
+        let serviceRtc = new ServiceRtc(channelName, account)
+        serviceRtc.add_privilege(ServiceRtc.kPrivilegeJoinChannel, expire)
+        if (role == Role.PUBLISHER) {
+            serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishAudioStream, expire)
+            serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishVideoStream, expire)
+            serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishDataStream, expire)
+        }
+        token.add_service(serviceRtc)
+
+        return token.build()
     }
 }
 
-module.exports.RtcTokenBuilder = RtcTokenBuilder;
-module.exports.Role = Role;
+module.exports.RtcTokenBuilder = RtcTokenBuilder
+module.exports.Role = Role
