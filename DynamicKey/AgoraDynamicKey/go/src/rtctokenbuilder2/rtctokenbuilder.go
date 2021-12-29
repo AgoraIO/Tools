90dc8466cbc9874f58ff5b2203962eb3cf03bfdd
lixinhui <lixinhui@agora.io>
2021-12-29 16:26:27 +0800

    support setting token and privilege expiration time

--- DynamicKey/AgoraDynamicKey/go/src/rtctokenbuilder2/rtctokenbuilder.go
+++ DynamicKey/AgoraDynamicKey/go/src/rtctokenbuilder2/rtctokenbuilder.go
@@ -1,64 +1,70 @@
 package rtctokenbuilder2
 
 import (
-    accesstoken "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/accesstoken2"
+	accesstoken "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/accesstoken2"
 )
 
 // Role type
 type Role uint16
 
 const (
-    RolePublisher  = 1 // for live broadcaster
-    RoleSubscriber = 2 // default, for live audience
+	RolePublisher  = 1 // for live broadcaster
+	RoleSubscriber = 2 // default, for live audience
 )
 
-// Build the RTC token with uid.
-//
-// appId:           The App ID issued to you by Agora. Apply for a new App ID from
-//                  Agora Dashboard if it is missing from your kit. See Get an App ID.
-// appCertificate:  Certificate of the application that you registered in
-//                  the Agora Dashboard. See Get an App Certificate.
-// channelName:     Unique channel name for the AgoraRTC session in the string format
-// uid:             User ID. A 32-bit unsigned integer with a value ranging from 1 to (232-1).
-//                  optionalUid must be unique.
-// role:            RolePublisher: A broadcaster/host in a live-broadcast profile.
-//                  RoleSubscriber: An audience(default) in a live-broadcast profile.
-// expire:          represented by the number of seconds elapsed since now. If, for example, you want to access the
-//                  Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+// BuildTokenWithUid Build the RTC token with uid.
+// appId: The App ID issued to you by Agora. Apply for a new App ID from
+//     Agora Dashboard if it is missing from your kit. See Get an App ID.
+// appCertificate: Certificate of the application that you registered in
+//     the Agora Dashboard. See Get an App Certificate.
+// channelName: Unique channel name for the AgoraRTC session in the string format
+// uid: User ID. A 32-bit unsigned integer with a value ranging from 1 to (232-1). optionalUid must be unique.
+// role: RolePublisher: A broadcaster/host in a live-broadcast profile.
+//     RoleSubscriber: An audience(default) in a live-broadcast profile.
+// expire: represented by the number of seconds elapsed since now. If, for example, you want to access the
+//     Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+// tokenExpire: represented by the number of seconds elapsed since now. If, for example,
+//     you want to access the Agora Service within 10 minutes after the token is generated,
+//     set token_expire as 600(seconds).
+// privilegeExpire: represented by the number of seconds elapsed since now. If, for example,
+//     you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).
 // return The RTC token.
-func BuildTokenWithUid(appId string, appCertificate string, channelName string, uid uint32, role Role, expire uint32) (string, error) {
-    return BuildTokenWithUserAccount(appId, appCertificate, channelName, accesstoken.GetUidStr(uid), role, expire)
+func BuildTokenWithUid(appId string, appCertificate string, channelName string, uid uint32, role Role, tokenExpire uint32, privilegeExpire uint32) (string, error) {
+	return BuildTokenWithUserAccount(appId, appCertificate, channelName, accesstoken.GetUidStr(uid), role, tokenExpire, privilegeExpire)
 }
 
-// Build the RTC token with account.
+// BuildTokenWithUserAccount Build the RTC token with account.
 //
-// appId:           The App ID issued to you by Agora. Apply for a new App ID from
-//                  Agora Dashboard if it is missing from your kit. See Get an App ID.
-// appCertificate:  Certificate of the application that you registered in
-//                  the Agora Dashboard. See Get an App Certificate.
-// channelName:     Unique channel name for the AgoraRTC session in the string format
-// account:         The user's account, max length is 255 Bytes.
-// role:            RolePublisher: A broadcaster/host in a live-broadcast profile.
-//                  RoleSubscriber: An audience(default) in a live-broadcast profile.
-// expire:          represented by the number of seconds elapsed since now. If, for example, you want to access the
-//                  Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+// appId: The App ID issued to you by Agora. Apply for a new App ID from
+//     Agora Dashboard if it is missing from your kit. See Get an App ID.
+// appCertificate: Certificate of the application that you registered in
+//     the Agora Dashboard. See Get an App Certificate.
+// channelName: Unique channel name for the AgoraRTC session in the string format
+// account: The user's account, max length is 255 Bytes.
+// role: RolePublisher: A broadcaster/host in a live-broadcast profile.
+//     RoleSubscriber: An audience(default) in a live-broadcast profile.
+// tokenExpire: represented by the number of seconds elapsed since now. If, for example,
+//     you want to access the Agora Service within 10 minutes after the token is generated,
+//     set token_expire as 600(seconds).
+// privilegeExpire: represented by the number of seconds elapsed since now. If, for example,
+//     you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).
 // return The RTC token.
-func BuildTokenWithUserAccount(appId string, appCertificate string, channelName string, account string, role Role, expire uint32) (string, error) {
-    token := accesstoken.NewAccessToken(appId, appCertificate, expire)
+func BuildTokenWithUserAccount(appId string, appCertificate string, channelName string, account string, role Role, tokenExpire uint32, privilegeExpire uint32) (string, error) {
+	token := accesstoken.NewAccessToken(appId, appCertificate, tokenExpire)
 
-    serviceRtc := accesstoken.NewServiceRtc(channelName, account)
-    serviceRtc.AddPrivilege(accesstoken.PrivilegeJoinChannel, expire)
-    if role == RolePublisher {
-        serviceRtc.AddPrivilege(accesstoken.PrivilegePublishAudioStream, expire)
-        serviceRtc.AddPrivilege(accesstoken.PrivilegePublishVideoStream, expire)
-        serviceRtc.AddPrivilege(accesstoken.PrivilegePublishDataStream, expire)
-    }
-    token.AddService(serviceRtc)
+	serviceRtc := accesstoken.NewServiceRtc(channelName, account)
+	serviceRtc.AddPrivilege(accesstoken.PrivilegeJoinChannel, privilegeExpire)
+	if role == RolePublisher {
+		serviceRtc.AddPrivilege(accesstoken.PrivilegePublishAudioStream, privilegeExpire)
+		serviceRtc.AddPrivilege(accesstoken.PrivilegePublishVideoStream, privilegeExpire)
+		serviceRtc.AddPrivilege(accesstoken.PrivilegePublishDataStream, privilegeExpire)
+	}
+	token.AddService(serviceRtc)
 
-    return token.Build()
+	return token.Build()
 }
 
-// Generates an RTC token with the specified privilege.
+// BuildTokenWithUidAndPrivilege Generates an RTC token with the specified privilege.
 //
 // This method supports generating a token with the following privileges:
 // - Joining an RTC channel.
@@ -109,12 +115,12 @@ func BuildTokenWithUserAccount(appId string, appCertificate string, channelName 
 // current timestamp plus 600 seconds, the token expires in 10 minutes. If you do not want to enable this privilege,
 // set pubDataStreamPrivilegeExpire as the current Unix timestamp.
 func BuildTokenWithUidAndPrivilege(appId string, appCertificate string, channelName string, uid uint32,
-    tokenExpire uint32, joinChannelPrivilegeExpire uint32, pubAudioPrivilegeExpire uint32, pubVideoPrivilegeExpire uint32, pubDataStreamPrivilegeExpire uint32) (string, error) {
-    return BuildTokenWithUserAccountAndPrivilege(appId, appCertificate, channelName, accesstoken.GetUidStr(uid), tokenExpire,
-        joinChannelPrivilegeExpire, pubAudioPrivilegeExpire, pubVideoPrivilegeExpire, pubDataStreamPrivilegeExpire)
+	tokenExpire uint32, joinChannelPrivilegeExpire uint32, pubAudioPrivilegeExpire uint32, pubVideoPrivilegeExpire uint32, pubDataStreamPrivilegeExpire uint32) (string, error) {
+	return BuildTokenWithUserAccountAndPrivilege(appId, appCertificate, channelName, accesstoken.GetUidStr(uid), tokenExpire,
+		joinChannelPrivilegeExpire, pubAudioPrivilegeExpire, pubVideoPrivilegeExpire, pubDataStreamPrivilegeExpire)
 }
 
-// Generates an RTC token with the specified privilege.
+// BuildTokenWithUserAccountAndPrivilege Generates an RTC token with the specified privilege.
 //
 // This method supports generating a token with the following privileges:
 // - Joining an RTC channel.
@@ -165,15 +171,15 @@ func BuildTokenWithUidAndPrivilege(appId string, appCertificate string, channelN
 // current timestamp plus 600 seconds, the token expires in 10 minutes. If you do not want to enable this privilege,
 // set pubDataStreamPrivilegeExpire as the current Unix timestamp.
 func BuildTokenWithUserAccountAndPrivilege(appId string, appCertificate string, channelName string, account string,
-    tokenExpire uint32, joinChannelPrivilegeExpire uint32, pubAudioPrivilegeExpire uint32, pubVideoPrivilegeExpire uint32, pubDataStreamPrivilegeExpire uint32) (string, error) {
-    token := accesstoken.NewAccessToken(appId, appCertificate, tokenExpire)
+	tokenExpire uint32, joinChannelPrivilegeExpire uint32, pubAudioPrivilegeExpire uint32, pubVideoPrivilegeExpire uint32, pubDataStreamPrivilegeExpire uint32) (string, error) {
+	token := accesstoken.NewAccessToken(appId, appCertificate, tokenExpire)
 
-    serviceRtc := accesstoken.NewServiceRtc(channelName, account)
-    serviceRtc.AddPrivilege(accesstoken.PrivilegeJoinChannel, joinChannelPrivilegeExpire)
-    serviceRtc.AddPrivilege(accesstoken.PrivilegePublishAudioStream, pubAudioPrivilegeExpire)
-    serviceRtc.AddPrivilege(accesstoken.PrivilegePublishVideoStream, pubVideoPrivilegeExpire)
-    serviceRtc.AddPrivilege(accesstoken.PrivilegePublishDataStream, pubDataStreamPrivilegeExpire)
-    token.AddService(serviceRtc)
+	serviceRtc := accesstoken.NewServiceRtc(channelName, account)
+	serviceRtc.AddPrivilege(accesstoken.PrivilegeJoinChannel, joinChannelPrivilegeExpire)
+	serviceRtc.AddPrivilege(accesstoken.PrivilegePublishAudioStream, pubAudioPrivilegeExpire)
+	serviceRtc.AddPrivilege(accesstoken.PrivilegePublishVideoStream, pubVideoPrivilegeExpire)
+	serviceRtc.AddPrivilege(accesstoken.PrivilegePublishDataStream, pubDataStreamPrivilegeExpire)
+	token.AddService(serviceRtc)
 
-    return token.Build()
+	return token.Build()
 }
