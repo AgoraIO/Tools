4fa4e1c2e5951e95145cff16e68bfdb91d2f6c1b
lixinhui <lixinhui@agora.io>
2021-12-30 15:08:08 +0800

    support setting token and privilege expiration time

--- DynamicKey/AgoraDynamicKey/java/src/main/java/io/agora/media/RtcTokenBuilder2.java
+++ DynamicKey/AgoraDynamicKey/java/src/main/java/io/agora/media/RtcTokenBuilder2.java
@@ -16,47 +16,53 @@ public class RtcTokenBuilder2 {
     /**
      * Build the RTC token with uid.
      *
-     * @param appId:          The App ID issued to you by Agora. Apply for a new App ID from
-     *                        Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param appCertificate: Certificate of the application that you registered in
-     *                        the Agora Dashboard. See Get an App Certificate.
-     * @param channelName:    Unique channel name for the AgoraRTC session in the string format
-     * @param uid:            User ID. A 32-bit unsigned integer with a value ranging from 1 to (232-1).
-     *                        optionalUid must be unique.
-     * @param role:           ROLE_PUBLISHER: A broadcaster/host in a live-broadcast profile.
-     *                        ROLE_SUBSCRIBER: An audience(default) in a live-broadcast profile.
-     * @param expire:         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                        Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param appId:            The App ID issued to you by Agora. Apply for a new App ID from
+     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param appCertificate:   Certificate of the application that you registered in
+     *                          the Agora Dashboard. See Get an App Certificate.
+     * @param channelName:      Unique channel name for the AgoraRTC session in the string format
+     * @param uid:              User ID. A 32-bit unsigned integer with a value ranging from 1 to (232-1).
+     *                          optionalUid must be unique.
+     * @param role:             ROLE_PUBLISHER: A broadcaster/host in a live-broadcast profile.
+     *                          ROLE_SUBSCRIBER: An audience(default) in a live-broadcast profile.
+     * @param token_expire:     represented by the number of seconds elapsed since now. If, for example,
+     *                          you want to access the Agora Service within 10 minutes after the token is generated,
+     *                          set token_expire as 600(seconds).
+     * @param privilege_expire: represented by the number of seconds elapsed since now. If, for example,
+     *                          you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).
      * @return The RTC token.
      */
-    public String buildTokenWithUid(String appId, String appCertificate, String channelName, int uid, Role role, int expire) {
-        return buildTokenWithUserAccount(appId, appCertificate, channelName, AccessToken2.getUidStr(uid), role, expire);
+    public String buildTokenWithUid(String appId, String appCertificate, String channelName, int uid, Role role, int token_expire, int privilege_expire) {
+        return buildTokenWithUserAccount(appId, appCertificate, channelName, AccessToken2.getUidStr(uid), role, token_expire, privilege_expire);
     }
 
     /**
      * Build the RTC token with account.
      *
-     * @param appId:          The App ID issued to you by Agora. Apply for a new App ID from
-     *                        Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param appCertificate: Certificate of the application that you registered in
-     *                        the Agora Dashboard. See Get an App Certificate.
-     * @param channelName:    Unique channel name for the AgoraRTC session in the string format
-     * @param account:        The user's account, max length is 255 Bytes.
-     * @param role:           ROLE_PUBLISHER: A broadcaster/host in a live-broadcast profile.
-     *                        ROLE_SUBSCRIBER: An audience(default) in a live-broadcast profile.
-     * @param expire:         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                        Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param appId:            The App ID issued to you by Agora. Apply for a new App ID from
+     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param appCertificate:   Certificate of the application that you registered in
+     *                          the Agora Dashboard. See Get an App Certificate.
+     * @param channelName:      Unique channel name for the AgoraRTC session in the string format
+     * @param account:          The user's account, max length is 255 Bytes.
+     * @param role:             ROLE_PUBLISHER: A broadcaster/host in a live-broadcast profile.
+     *                          ROLE_SUBSCRIBER: An audience(default) in a live-broadcast profile.
+     * @param token_expire:     represented by the number of seconds elapsed since now. If, for example,
+     *                          you want to access the Agora Service within 10 minutes after the token is generated,
+     *                          set token_expire as 600(seconds).
+     * @param privilege_expire: represented by the number of seconds elapsed since now. If, for example,
+     *                          you want to enable your privilege for 10 minutes, set privilege_expire as 600(seconds).
      * @return The RTC token.
      */
-    public String buildTokenWithUserAccount(String appId, String appCertificate, String channelName, String account, Role role, int expire) {
-        AccessToken2 accessToken = new AccessToken2(appId, appCertificate, expire);
+    public String buildTokenWithUserAccount(String appId, String appCertificate, String channelName, String account, Role role, int token_expire, int privilege_expire) {
+        AccessToken2 accessToken = new AccessToken2(appId, appCertificate, token_expire);
         AccessToken2.Service serviceRtc = new AccessToken2.ServiceRtc(channelName, account);
 
-        serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_JOIN_CHANNEL, expire);
+        serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_JOIN_CHANNEL, privilege_expire);
         if (role == Role.ROLE_PUBLISHER) {
-            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_AUDIO_STREAM, expire);
-            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_VIDEO_STREAM, expire);
-            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_DATA_STREAM, expire);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_AUDIO_STREAM, privilege_expire);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_VIDEO_STREAM, privilege_expire);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_DATA_STREAM, privilege_expire);
         }
         accessToken.addService(serviceRtc);
 
--- DynamicKey/AgoraDynamicKey/java/src/main/java/io/agora/sample/RtcTokenBuilder2Sample.java
+++ DynamicKey/AgoraDynamicKey/java/src/main/java/io/agora/sample/RtcTokenBuilder2Sample.java
@@ -9,20 +9,21 @@ public class RtcTokenBuilder2Sample {
     static String channelName = "7d72365eb983485397e3e3f9d460bdda";
     static String account = "2082341273";
     static int uid = 2082341273;
-    static int expirationInSeconds = 3600;
+    static int tokenExpirationInSeconds = 3600;
+    static int privilegeExpirationInSeconds = 3600;
 
     public static void main(String[] args) {
         RtcTokenBuilder2 token = new RtcTokenBuilder2();
-        String result = token.buildTokenWithUid(appId, appCertificate, channelName, uid, Role.ROLE_SUBSCRIBER, expirationInSeconds);
+        String result = token.buildTokenWithUid(appId, appCertificate, channelName, uid, Role.ROLE_SUBSCRIBER, tokenExpirationInSeconds, privilegeExpirationInSeconds);
         System.out.println("Token with uid: " + result);
 
-        result = token.buildTokenWithUserAccount(appId, appCertificate, channelName, account, Role.ROLE_SUBSCRIBER, expirationInSeconds);
+        result = token.buildTokenWithUserAccount(appId, appCertificate, channelName, account, Role.ROLE_SUBSCRIBER, tokenExpirationInSeconds, privilegeExpirationInSeconds);
         System.out.println("Token with account: " + result);
 
-        result = token.buildTokenWithUid(appId, appCertificate, channelName, uid, expirationInSeconds, expirationInSeconds, expirationInSeconds, expirationInSeconds, expirationInSeconds);
+        result = token.buildTokenWithUid(appId, appCertificate, channelName, uid, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds);
         System.out.println("Token with uid and privilege: " + result);
 
-        result = token.buildTokenWithUserAccount(appId, appCertificate, channelName, account, expirationInSeconds, expirationInSeconds, expirationInSeconds, expirationInSeconds, expirationInSeconds);
+        result = token.buildTokenWithUserAccount(appId, appCertificate, channelName, account, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds, privilegeExpirationInSeconds);
         System.out.println("Token with account and privilege: " + result);
     }
 }
