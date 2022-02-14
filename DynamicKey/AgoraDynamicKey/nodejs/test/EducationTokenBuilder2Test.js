cb3d172dededc6b5cf11f4945fb003fef6b81f05
liwenjie <liwenjie@agora.io>
2022-02-14 14:18:12 +0800

    update userUuid

--- DynamicKey/AgoraDynamicKey/nodejs/test/EducationTokenBuilder2Test.js
+++ DynamicKey/AgoraDynamicKey/nodejs/test/EducationTokenBuilder2Test.js
@@ -9,7 +9,7 @@ const appId = "970CA35de60c44645bbae8a215061b33"
 const appCertificate = "5CFd2fd1755d40ecb72977518be15d3b"
 const expire = 600
 const roomUuid = "123";
-const userUuid = "jasoncai1";
+const userUuid = "2882341273";
 const role = 1;
 
  exports.BuildRoomUserToken_Test = function (test) {
