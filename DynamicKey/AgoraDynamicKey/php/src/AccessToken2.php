70070e006fe08b5df4906e606af0b555ba4ac2c9
lixinhui <lixinhui@agora.io>
2022-02-22 15:48:02 +0800

    chore(php): remove chat_user_id

--- DynamicKey/AgoraDynamicKey/php/src/AccessToken2.php
+++ DynamicKey/AgoraDynamicKey/php/src/AccessToken2.php
@@ -170,22 +170,20 @@ class ServiceEducation extends Service
 
     public $roomUuid;
     public $userUuid;
-    public $chatUserId;
     public $role;
 
 
-    public function __construct($roomUuid = "", $userUuid = "", $chatUserId = "", $role = -1)
+    public function __construct($roomUuid = "", $userUuid = "", $role = -1)
     {
         parent::__construct(self::SERVICE_TYPE);
         $this->roomUuid = $roomUuid;
         $this->userUuid = $userUuid;
-        $this->chatUserId = $chatUserId;
         $this->role = $role;
     }
 
     public function pack()
     {
-        return parent::pack() . Util::packString($this->roomUuid) . Util::packString($this->userUuid) . Util::packString($this->chatUserId) . Util::packInt16($this->role);
+        return parent::pack() . Util::packString($this->roomUuid) . Util::packString($this->userUuid) . Util::packInt16($this->role);
     }
 
     public function unpack(&$data)
@@ -193,7 +191,6 @@ class ServiceEducation extends Service
         parent::unpack($data);
         $this->roomUuid = Util::unpackString($data);
         $this->userUuid = Util::unpackString($data);
-        $this->chatUserId = Util::unpackString($data);
         $this->role = Util::unpackInt16($data);
     }
 }
--- DynamicKey/AgoraDynamicKey/php/src/EducationTokenBuilder.php
+++ DynamicKey/AgoraDynamicKey/php/src/EducationTokenBuilder.php
@@ -7,23 +7,23 @@ class EducationTokenBuilder
     /**
      * Build user room token.
      *
-     * @param $appId :          The App ID issued to you by Agora. Apply for a new App ID from
-     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param $appCertificate : Certificate of the application that you registered in
-     *                          the Agora Dashboard. See Get an App Certificate.
-     * @param $roomUuid :       The room's id, must be unique.
-     * @param $userUuid :       The user's id, must be unique.
-     * @param $role :           The user's role, such as 0(invisible), 1(teacher), 2(student), 3(assistant), 4(observer) etc.
-     * @param $expire :         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                          Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param $appId The App ID issued to you by Agora. Apply for a new App ID from
+     *     Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param $appCertificate Certificate of the application that you registered in
+     *     the Agora Dashboard. See Get an App Certificate.
+     * @param $roomUuid The room's id, must be unique.
+     * @param $userUuid The user's id, must be unique.
+     * @param $role The user's role, such as 0(invisible), 1(teacher), 2(student), 3(assistant), 4(observer) etc.
+     * @param $expire represented by the number of seconds elapsed since now. If, for example, you want to access the
+     *     Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
      * @return The user room token.
      */
     public static function buildRoomUserToken($appId, $appCertificate, $roomUuid, $userUuid, $role, $expire)
     {
         $accessToken = new AccessToken2($appId, $appCertificate, $expire);
-        $chatUserId = md5($userUuid);
 
-        $serviceEducation = new ServiceEducation($roomUuid, $userUuid, $chatUserId, $role);
+        $chatUserId = md5($userUuid);
+        $serviceEducation = new ServiceEducation($roomUuid, $userUuid, $role);
         $serviceEducation->addPrivilege($serviceEducation::PRIVILEGE_ROOM_USER, $expire);
         $accessToken->addService($serviceEducation);
 
@@ -41,19 +41,19 @@ class EducationTokenBuilder
     /**
      * Build user individual token.
      *
-     * @param $appId :          The App ID issued to you by Agora. Apply for a new App ID from
-     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param $appCertificate : Certificate of the application that you registered in
-     *                          the Agora Dashboard. See Get an App Certificate.
-     * @param $userUuid :       The user's id, must be unique.
-     * @param $expire :         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                          Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param $appId The App ID issued to you by Agora. Apply for a new App ID from
+     *     Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param $appCertificate Certificate of the application that you registered in
+     *     the Agora Dashboard. See Get an App Certificate.
+     * @param $userUuid The user's id, must be unique.
+     * @param $expire represented by the number of seconds elapsed since now. If, for example, you want to access the
+     *     Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
      * @return The build user individual token.
      */
     public static function buildUserToken($appId, $appCertificate, $userUuid, $expire)
     {
         $accessToken = new AccessToken2($appId, $appCertificate, $expire);
-        $serviceEducation = new ServiceEducation("", $userUuid, "", -1);
+        $serviceEducation = new ServiceEducation("", $userUuid);
         $serviceEducation->addPrivilege($serviceEducation::PRIVILEGE_USER, $expire);
         $accessToken->addService($serviceEducation);
 
@@ -63,12 +63,12 @@ class EducationTokenBuilder
     /**
      * Build app global token.
      *
-     * @param $appId :          The App ID issued to you by Agora. Apply for a new App ID from
-     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param $appCertificate : Certificate of the application that you registered in
-     *                          the Agora Dashboard. See Get an App Certificate.
-     * @param $expire :         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                          Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param $appId The App ID issued to you by Agora. Apply for a new App ID from
+     *     Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param $appCertificate Certificate of the application that you registered in
+     *     the Agora Dashboard. See Get an App Certificate.
+     * @param $expire represented by the number of seconds elapsed since now. If, for example, you want to access the
+     *     Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
      * @return The app global token.
      */
     public static function buildAppToken($appId, $appCertificate, $expire)
