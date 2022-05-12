fe27d28540415bd188df498c8b65ea70a39a416a
mergitto <riki.akg@gmail.com>
2022-05-12 14:29:09 +0900

    fix RtcTokenBuilder.go comment

--- DynamicKey/AgoraDynamicKey/go/src/RtcTokenBuilder/RtcTokenBuilder.go
+++ DynamicKey/AgoraDynamicKey/go/src/RtcTokenBuilder/RtcTokenBuilder.go
@@ -26,8 +26,7 @@ type RtcTokenBuilder struct {
 // appCertificate:	Certificate of the application that you registered in
 //                  the Agora Dashboard. See Get an App Certificate.
 // channelName:Unique channel name for the AgoraRTC session in the string format
-// uid: User ID. A 32-bit unsigned integer with a value ranging from
-//      1 to (232-1). optionalUid must be unique.
+// userAccount: The user account.
 // role: Role_Publisher = 1: A broadcaster (host) in a live-broadcast profile.
 //       Role_Subscriber = 2: (Default) A audience in a live-broadcast profile.
 // privilegeExpireTs: represented by the number of seconds elapsed since
@@ -53,7 +52,8 @@ func BuildTokenWithUserAccount(appID string, appCertificate string, channelName 
 // appCertificate:	Certificate of the application that you registered in
 //                  the Agora Dashboard. See Get an App Certificate.
 // channelName:Unique channel name for the AgoraRTC session in the string format
-// userAccount: The user account.
+// uid: User ID. A 32-bit unsigned integer with a value ranging from
+//      1 to (232-1). optionalUid must be unique.
 // role: Role_Publisher = 1: A broadcaster (host) in a live-broadcast profile.
 //       Role_Subscriber = 2: (Default) A audience in a live-broadcast profile.
 // privilegeExpireTs: represented by the number of seconds elapsed since
