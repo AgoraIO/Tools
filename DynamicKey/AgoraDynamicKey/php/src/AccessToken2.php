114efc8b2e20720f31576dc2bf4687bf60c3cf52
lixinhui <lixinhui@agora.io>
2022-07-18 21:44:56 +0800

    chore(): remove service type streaming

--- DynamicKey/AgoraDynamicKey/php/src/AccessToken2.php
+++ DynamicKey/AgoraDynamicKey/php/src/AccessToken2.php
@@ -87,34 +87,6 @@ class ServiceRtm extends Service
     }
 }
 
-class ServiceStreaming extends Service
-{
-    const SERVICE_TYPE = 3;
-    const PRIVILEGE_PUBLISH_MIX_STREAM = 1;
-    const PRIVILEGE_PUBLISH_RAW_STREAM = 2;
-    public $channelName;
-    public $uid;
-
-    public function __construct($channelName = "", $uid = "")
-    {
-        parent::__construct(self::SERVICE_TYPE);
-        $this->channelName = $channelName;
-        $this->uid = $uid;
-    }
-
-    public function pack()
-    {
-        return parent::pack() . Util::packString($this->channelName) . Util::packString($this->uid);
-    }
-
-    public function unpack(&$data)
-    {
-        parent::unpack($data);
-        $this->channelName = Util::unpackString($data);
-        $this->uid = Util::unpackString($data);
-    }
-}
-
 class ServiceFpa extends Service
 {
     const SERVICE_TYPE = 4;
@@ -275,7 +247,6 @@ class AccessToken2
         $servicesObj = [
             ServiceRtc::SERVICE_TYPE => new ServiceRtc(),
             ServiceRtm::SERVICE_TYPE => new ServiceRtm(),
-            ServiceStreaming::SERVICE_TYPE => new ServiceStreaming(),
             ServiceFpa::SERVICE_TYPE => new ServiceFpa(),
             ServiceChat::SERVICE_TYPE => new ServiceChat(),
             ServiceEducation::SERVICE_TYPE => new ServiceEducation(),
