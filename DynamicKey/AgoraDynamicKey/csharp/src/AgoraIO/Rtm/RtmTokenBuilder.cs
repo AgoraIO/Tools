190925487f2d04781df110530f0429692ae970d9
lixinhui <lixinhui@agora.io>
2022-08-10 21:29:32 +0800

    chore(c#): add builder and sample for RTC/RTM

--- DynamicKey/AgoraDynamicKey/csharp/src/AgoraIO/Rtm/RtmTokenBuilder.cs
+++ DynamicKey/AgoraDynamicKey/csharp/src/AgoraIO/Rtm/RtmTokenBuilder.cs
@@ -7,14 +7,28 @@ namespace AgoraIO.Rtm
 {
     public class RtmTokenBuilder
     {
-
-        public AccessToken mTokenCreator;
-
-        public string buildToken(string appId, string appCertificate, string uid, uint privilegeTs)
+        public enum Role
         {
-            mTokenCreator = new AccessToken(appId, appCertificate, uid, "");
-            mTokenCreator.addPrivilege(Privileges.kRtmLogin, privilegeTs);
-            return mTokenCreator.build();
+            RoleRtmUser = 1,
+        }
+
+        // buildToken
+        // appID: The App ID issued to you by Agora. Apply for a new App ID from
+        //        Agora Dashboard if it is missing from your kit. See Get an App ID.
+        // appCertificate:	Certificate of the application that you registered in
+        //                  the Agora Dashboard. See Get an App Certificate.
+        // userAccount: The user account.
+        // privilegeExpireTs: represented by the number of seconds elapsed since
+        //                    1/1/1970. If, for example, you want to access the
+        //                    Agora Service within 10 minutes after the token is
+        //                    generated, set expireTimestamp as the current
+        //                    timestamp + 600 (seconds)./
+        public static string buildToken(string appId, string appCertificate, string userAccount, uint privilegeExpiredTs)
+        {
+            AccessToken accessToken = new AccessToken(appId, appCertificate, userAccount, "");
+            accessToken.addPrivilege(Privileges.kRtmLogin, privilegeExpiredTs);
+
+            return accessToken.build();
         }
     }
 }
