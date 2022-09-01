2626ea3985a222b693562c7cfe94bfdbe0611119
sunshinexcode <24xinhui@163.com>
2022-09-01 18:00:05 +0800

    chore(): modify comment

--- DynamicKey/AgoraDynamicKey/nodejs/src/RtmTokenBuilder2.js
+++ DynamicKey/AgoraDynamicKey/nodejs/src/RtmTokenBuilder2.js
@@ -5,13 +5,13 @@ class RtmTokenBuilder {
     /**
      * Build the RTM token.
      *
-     * @param $appId :          The App ID issued to you by Agora. Apply for a new App ID from
-     *                          Agora Dashboard if it is missing from your kit. See Get an App ID.
-     * @param $appCertificate : Certificate of the application that you registered in
-     *                          the Agora Dashboard. See Get an App Certificate.
-     * @param $userId :         The user's account, max length is 64 Bytes.
-     * @param $expire :         represented by the number of seconds elapsed since now. If, for example, you want to access the
-     *                          Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
+     * @param appId The App ID issued to you by Agora. Apply for a new App ID from
+     * Agora Dashboard if it is missing from your kit. See Get an App ID.
+     * @param appCertificate Certificate of the application that you registered in
+     * the Agora Dashboard. See Get an App Certificate.
+     * @param userId The user's account, max length is 64 Bytes.
+     * @param expire represented by the number of seconds elapsed since now. If, for example, you want to access the
+     * Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
      * @return The RTM token.
      */
     static buildToken(appId, appCertificate, userId, expire) {
