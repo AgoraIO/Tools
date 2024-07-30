2809b794bbe446bc499e18c1bb197ae63a9a8346
sunshinexcode <24xinhui@163.com>
2024-07-31 00:55:32 +0800

    chore(): format code

--- DynamicKey/AgoraDynamicKey/rust/src/fpa_token_builder.rs
+++ DynamicKey/AgoraDynamicKey/rust/src/fpa_token_builder.rs
@@ -7,16 +7,11 @@ use crate::access_token;
 // app_certificate: Certificate of the application that you registered in
 //                  the Agora Dashboard. See Get an App Certificate.
 // return The FPA token.
-pub fn build_token(
-    app_id: &str,
-    app_certificate: &str,
-) -> Result<String, Box<dyn std::error::Error>> {
+pub fn build_token(app_id: &str, app_certificate: &str) -> Result<String, Box<dyn std::error::Error>> {
     let mut token = access_token::new_access_token(app_id, app_certificate, 24 * 3600);
 
     let mut service_fpa = access_token::new_service_fpa();
-    service_fpa
-        .service
-        .add_privilege(access_token::PRIVILEGE_LOGIN, 0);
+    service_fpa.service.add_privilege(access_token::PRIVILEGE_LOGIN, 0);
     token.add_service(Box::new(service_fpa));
 
     return token.build();
--- DynamicKey/AgoraDynamicKey/rust/src/rtm_token_builder.rs
+++ DynamicKey/AgoraDynamicKey/rust/src/rtm_token_builder.rs
@@ -10,18 +10,11 @@ use crate::access_token;
 // expire:          represented by the number of seconds elapsed since now. If, for example, you want to access the
 //                  Agora Service within 10 minutes after the token is generated, set expireTimestamp as 600(seconds).
 // return The RTM token.
-pub fn build_token(
-    app_id: &str,
-    app_certificate: &str,
-    user_id: &str,
-    expire: u32,
-) -> Result<String, Box<dyn std::error::Error>> {
+pub fn build_token(app_id: &str, app_certificate: &str, user_id: &str, expire: u32) -> Result<String, Box<dyn std::error::Error>> {
     let mut token = access_token::new_access_token(app_id, app_certificate, expire);
 
     let mut service_rtm = access_token::new_service_rtm(user_id);
-    service_rtm
-        .service
-        .add_privilege(access_token::PRIVILEGE_LOGIN, expire);
+    service_rtm.service.add_privilege(access_token::PRIVILEGE_LOGIN, expire);
     token.add_service(Box::new(service_rtm));
 
     return token.build();
