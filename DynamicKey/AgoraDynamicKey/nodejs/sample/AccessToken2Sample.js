ea9e2372e9ba6ee9e16c77bab2b89a14088d3bfa
sunshinexcode <24xinhui@163.com>
2025-04-23 16:31:53 +0800

    fix(): fix sample

--- DynamicKey/AgoraDynamicKey/nodejs/sample/AccessToken2Sample.js
+++ DynamicKey/AgoraDynamicKey/nodejs/sample/AccessToken2Sample.js
@@ -1,26 +1,36 @@
-const { AccessToken2, ServiceRtc } = require("../src/AccessToken2");
-const RtcRole = require("../src/RtcTokenBuilder").Role;
+const { AccessToken2, ServiceRtc } = require('../src/AccessToken2')
+const RtcRole = require('../src/RtcTokenBuilder2').Role
 
 // Need to set environment variable AGORA_APP_ID
-const appId = process.env.AGORA_APP_ID;
+const appId = process.env.AGORA_APP_ID
 // Need to set environment variable AGORA_APP_CERTIFICATE
-const appCertificate = process.env.AGORA_APP_CERTIFICATE;
+const appCertificate = process.env.AGORA_APP_CERTIFICATE
 
-const channelName = "7d72365eb983485397e3e3f9d460bdda";
-const uid = 2882341273;
-const account = "2882341273";
-const role = RtcRole.PUBLISHER;
-const expirationTimeInSeconds = 3600;
-const currentTimestamp = Math.floor(Date.now() / 1000);
+const channelName = '7d72365eb983485397e3e3f9d460bdda'
+const uid = 2882341273
+// const account = '2882341273'
+const role = RtcRole.PUBLISHER
+const tokenExpirationInSecond = 3600
+const joinChannelPrivilegeExpireInSeconds = 3600
+const pubAudioPrivilegeExpireInSeconds = 3600
+const pubVideoPrivilegeExpireInSeconds = 3600
+const pubDataStreamPrivilegeExpireInSeconds = 3600
 
-console.log("App Id:", appId);
-console.log("App Certificate:", appCertificate);
-if (appId == undefined || appId == "" || appCertificate == undefined || appCertificate == "") {
-    console.log("Need to set environment variable AGORA_APP_ID and AGORA_APP_CERTIFICATE");
-    process.exit(1);
+console.log('App Id:', appId)
+console.log('App Certificate:', appCertificate)
+if (appId == undefined || appId == '' || appCertificate == undefined || appCertificate == '') {
+    console.log('Need to set environment variable AGORA_APP_ID and AGORA_APP_CERTIFICATE')
+    process.exit(1)
 }
 
-let token = new AccessToken2(appId, appCertificate, currentTimestamp, expirationTimeInSeconds);
-let rtc_service = new ServiceRtc(channelName, uid);
-token.add_service(rtc_service);
-console.log("Token: ", token.build());
+let token = new AccessToken2(appId, appCertificate, 0, tokenExpirationInSecond)
+let serviceRtc = new ServiceRtc(channelName, uid)
+serviceRtc.add_privilege(ServiceRtc.kPrivilegeJoinChannel, joinChannelPrivilegeExpireInSeconds)
+if (role == RtcRole.PUBLISHER) {
+    serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishAudioStream, pubAudioPrivilegeExpireInSeconds)
+    serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishVideoStream, pubVideoPrivilegeExpireInSeconds)
+    serviceRtc.add_privilege(ServiceRtc.kPrivilegePublishDataStream, pubDataStreamPrivilegeExpireInSeconds)
+}
+token.add_service(serviceRtc)
+
+console.log('Token: ', token.build())
