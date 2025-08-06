766ff9caeed5658b808405490d463bcdf1f9032f
aldam70 <aldam70@126.com>
2025-08-06 19:31:09 +0800

    Update RtmTokenBuilder2.h to support rtm token2

--- DynamicKey/AgoraDynamicKey/cpp/src/RtmTokenBuilder2.h
+++ DynamicKey/AgoraDynamicKey/cpp/src/RtmTokenBuilder2.h
@@ -26,10 +26,38 @@ class RtmTokenBuilder2 {
    @return The new Token.
    */
   static std::string BuildToken(const std::string& app_id, const std::string& app_certificate, const std::string& user_id, uint32_t expire = 0);
+  /**
+   Builds an RTM token.
+
+   @param app_id The App ID issued to you by Agora.
+   @param app_certificate Certificate of the application that you registered in
+   the Agora Dashboard.
+   @param user_id The user's account, max length is 64 Bytes.
+   @param permissions Specify the permissions
+   @param expire represented by the number of seconds elapsed since now. If, for
+   example, you want to access the Agora Service within 10 minutes after the
+   token is generated, set expire as 600(seconds).
+   @return The new Token.
+   */
+  static std::string BuildToken(const std::string& app_id, const std::string& app_certificate, const std::string& user_id,
+      const ServiceRtm2::Permissions &permissions, uint32_t expire = 0);
+
 };
 
 inline std::string RtmTokenBuilder2::BuildToken(const std::string& app_id, const std::string& app_certificate, const std::string& user_id, uint32_t expire) {
   std::unique_ptr<Service> service(new ServiceRtm(user_id));
+  service->AddPrivilege(ServiceRtm::kPrivilegeLogin, expire);
+
+  AccessToken2 generator(app_id, app_certificate, 0, expire);
+  generator.AddService(std::move(service));
+
+  return generator.Build();
+}
+
+inline std::string RtmTokenBuilder2::BuildToken(const std::string& app_id, const std::string& app_certificate,
+    const std::string& user_id, const ServiceRtm2::Permissions &permissions,
+    uint32_t expire) {
+  std::unique_ptr<Service> service(new ServiceRtm2(user_id, permissions));
   service->AddPrivilege(ServiceRtm::kPrivilegeLogin, expire);
 
   AccessToken2 generator(app_id, app_certificate, 0, expire);
